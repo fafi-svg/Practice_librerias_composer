2da1592376b6b9e4e1bcbde08be9b27520116c85
fafi-svg <135574549+fafi-svg@users.noreply.github.com>
2023-09-21 15:08:27 -0500

    Conexion db y sintaxias consulta

--- index.php
+++ index.php
@@ -12,7 +12,11 @@
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
         echo $_ENV["USER"];
-        require __DIR__."\conexion.php";
+        require_once __DIR__ . "/conexion.php";
+        $query = $mysqli->query("SELECT employee_id FROM employees limit 10");
+        foreach($query as $id){
+            print_r ($id);
+        }
     ?>
 
 </body>
