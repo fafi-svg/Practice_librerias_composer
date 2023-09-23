f955d2aaf95c2a9ac07efd7c08dc9f5e9aced448
fafi-svg <rfaelsolanob@gmail.com>
2023-09-23 16:36:44 -0500

    concultas resubida

--- index.php
+++ index.php
@@ -13,13 +13,24 @@
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
         require_once __DIR__ . "/conexion.php";
-        $query = $mysqli->query("SELECT * FROM employees as e limit 15");
+        $query = $mysqli->query("select e.employee_id as 'ID EMPLEADO', e.first_name, e.last_name, e.email, e.phone_number, d.department_name, ej.first_name as 'Manager name', ej.email as 'Manager email'
+        from employees as e
+        left join departments as d
+        on d.department_id = e.department_id
+        left join employees as ej
+        on e.manager_id = ej.employee_id;");
         foreach($query as $id){
         ?>
             <div class="row">
                 <?php
                     foreach($id as $item){
-                       echo  "<div class=row__item>$item</div>";
+                        if ($item != null){
+                            echo  "<div class=row__item>$item</div>";
+                        }
+                        else{
+                            echo  "<div class=row__item>Null</div>";
+                        }
+                    
                     }
                 ?>
             </div>
